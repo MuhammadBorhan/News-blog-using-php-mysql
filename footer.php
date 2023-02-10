3582b5571c28476e16064f3264fd801168f9b51b
MuhammadBorhan <0317371981@student.wub.edu.bd>
2023-02-10 17:51:37 +0600

    first commit

---
+++ footer.php
@@ -0,0 +1,16 @@
+<!-- Footer -->
+<div >
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-md-12 text-center blog-footer">
+                <span>© Copyright <?php echo date('Y') ?> News-Blog | All Rights Reserved by <a target="_blank" href="https://borhanportfolio.netlify.app/">Borhan</a></span>
+            </div>
+        </div>
+    </div>
+</div>
+<!-- /Footer -->
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+
+  </body>
+</html>
---
+++ header.php
@@ -0,0 +1,41 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    
+    <!-- Custom stlylesheet -->
+    <link rel="stylesheet" href="css/style.css">
+    <title>News Blog</title>
+  </head>
+  <body>
+
+  <nav class="navbar navbar-expand-lg navbar-light bg-light blog-nav">
+  <div class="container-fluid">
+    <a class="navbar-brand fs-2 text-primary fw-bold" href="#">News Blog</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav ms-auto blog-menu">
+        <li>
+            <a href="users.php">Users</a>
+        </li>
+        <li>
+            <a href="category.php">Category</a>
+        </li>
+        <li>
+            <a href="category.php">Post</a>
+        </li>
+        <li>
+            <a href="#">Logout</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
